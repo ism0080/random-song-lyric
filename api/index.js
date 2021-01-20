47b5066dd5eeca8e5d7a33d0b0b4eaeab7346706
Isaac Mackle <isaac.mackle@dominos.com.au>
2021-01-20 15:56:41 +1300

    chore: init

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const Genius = require("genius-lyrics");
+const Client = new Genius.Client();
+
+const retrieveLyrics = async (artist) => {
+    const songs = await Client.songs.search(artist);
+
+    const indexSongs = Math.floor(Math.random() * Math.floor(songs.length));
+    const lyrics = await songs[indexSongs].lyrics();
+    const title = songs[indexSongs].fullTitle;
+    const image = songs[indexSongs].image;
+
+    const arrLyrics = lyrics
+        .split("\n")
+        .filter((l) => l.length && l[0] !== "[");
+    const indexLyrics = Math.floor(
+        Math.random() * Math.floor(arrLyrics.length)
+    );
+    return { lyrics: arrLyrics[indexLyrics], title, image };
+};
+
+module.exports = async (req, res) => {
+    try {
+        res.setHeader("content-type", "application/json");
+        res.send({
+            status: 200,
+            info: await retrieveLyrics(req.query.artist)
+        });
+    } catch (err) {
+        res.send({
+            status: 500,
+            message: err
+        });
+    }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Col, Container, Media, Row, Spinner } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import styles from "./App.module.css";
+
+export const App = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const [lyric, setLyric] = useState("");
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async (data: any, e: any) => {
+        setLoading(true);
+        const params = new URLSearchParams({ artist: data.artist });
+        fetch(`/api?${params}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => res.json())
+            .then(({ info, status }) => {
+                if (status === 500) {
+                    setLoading(false);
+                    setError(true);
+                    setTimeout(() => {
+                        setError(false);
+                    }, 2000);
+                } else {
+                    setLoading(false);
+                    setLyric(info.lyrics);
+                    setTitle(info.title);
+                    setImage(info.image);
+                }
+            });
+    };
+
+    return (
+        <div className={styles.App}>
+            <Container fluid>
+                {error && (
+                    <div
+                        className={`alert fade show d-flex alert-danger`}
+                        role="alert"
+                    >
+                        <p>Something went wrong</p>
+                        <span
+                            aria-hidden="true"
+                            className="ml-auto cursor-pointer"
+                            onClick={() => setError(false)}
+                        >
+                            &times;
+                        </span>
+                    </div>
+                )}
+                <h2>Random Song Lyric</h2>
+                <Row>
+                    <Col md={6} sm={12} xs={12}>
+                        <div className={styles.formWrapper}>
+                            <form
+                                onSubmit={handleSubmit(onSubmit)}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div className="form-group">
+                                    <label htmlFor="inputForArtist">
+                                        Artist
+                                    </label>
+                                    <input
+                                        id="inputForArtist"
+                                        name="artist"
+                                        type="text"
+                                        className="form-control"
+                                        aria-describedby="Enter artist"
+                                        placeholder="Enter Artist's Name"
+                                        ref={register({
+                                            required: {
+                                                value: true,
+                                                message:
+                                                    "Please enter an Artist"
+                                            }
+                                        })}
+                                    />
+                                    {errors.artist && (
+                                        <span
+                                            className={`${styles.errorMessage} mandatory`}
+                                        >
+                                            {errors.artist.message}
+                                        </span>
+                                    )}
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-outline-primary"
+                                >
+                                    {loading ? (
+                                        <Spinner
+                                            as="span"
+                                            animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                        />
+                                    ) : (
+                                        "Get Lyric"
+                                    )}
+                                </button>
+                            </form>
+                        </div>
+                    </Col>
+                    {lyric.length > 0 && (
+                        <Col
+                            md={6}
+                            sm={12}
+                            xs={12}
+                            className={styles.lyricWrapper}
+                        >
+                            <Media>
+                                <img
+                                    width={100}
+                                    height={100}
+                                    className="mr-3"
+                                    src={image}
+                                    alt={title}
+                                />
+                                <Media.Body>
+                                    <h5 className={styles.truncate}>{title}</h5>
+                                    <em>{lyric}</em>
+                                </Media.Body>
+                            </Media>
+                        </Col>
+                    )}
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
 export default App;
