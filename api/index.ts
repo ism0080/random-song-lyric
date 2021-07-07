51eaaa70d42062fc54c63fdccc49d3d3c8da1e8f
Isaac Mackle <isaac.mackle@dominos.com.au>
2021-07-07 14:54:45 +1200

    linting

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import Genius from 'genius-lyrics'
+
 const Client = new Genius.Client(process.env.GENIUS_KEY)
 
 const retrieveLyrics = async (artist: string) => {
@@ -8,10 +9,11 @@ const retrieveLyrics = async (artist: string) => {
   const indexSongs = Math.floor(Math.random() * Math.floor(songs.length))
   const lyrics = await songs[indexSongs].lyrics()
   const title = songs[indexSongs].fullTitle
-  const image = songs[indexSongs].image
+  const { image } = songs[indexSongs]
 
   const arrLyrics = lyrics.split('\n').filter((l: string) => l.length && l[0] !== '[')
   const indexLyrics = Math.floor(Math.random() * Math.floor(arrLyrics.length))
+
   return { lyrics: arrLyrics[indexLyrics], title, image }
 }
 
